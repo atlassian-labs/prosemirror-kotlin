c5e983fea218fab3c59ab45c951bafb490b2f96a
Douglas Marques <dmarques@atlassian.com>
2024-08-20 13:02:04 +1000

    Fixing issue

--- src/main/kotlin/com/atlassian/prosemirror/util/Tools.kt
+++ src/main/kotlin/com/atlassian/prosemirror/util/Tools.kt
@@ -3,6 +3,8 @@ package com.atlassian.prosemirror.util
 import kotlin.math.max
 import kotlin.math.min
 
+var safeMode = true
+
 fun String.slice(from: Int, to: Int): String {
     return substring(max(0, from), min(to, length))
 }
--- src/main/kotlin/com/atlassian/prosemirror/util/Utils.kt
+++ src/main/kotlin/com/atlassian/prosemirror/util/Utils.kt
@@ -7,23 +7,21 @@ import com.atlassian.prosemirror.model.ResolvedPos
 fun getMilliseconds() = System.currentTimeMillis()
 
 fun Node.resolveAndLog(from: Int, to: Int): Pair<ResolvedPos, ResolvedPos>? {
-  try {
-    val resolvedFrom = resolve(from)
-    val resolvedTo = resolve(to)
-    return resolvedFrom to resolvedTo
-  } catch (e: RangeError) {
-    // It's safe to log Node.toString as UGC is not returned in Debug mode
-    // TODO: add library to log
-    return null
-  }
+    try {
+        val resolvedFrom = resolve(from)
+        val resolvedTo = resolve(to)
+        return resolvedFrom to resolvedTo
+    } catch (e: RangeError) {
+        if (!safeMode) throw e
+        return null
+    }
 }
 
 fun Node.resolveAndLog(pos: Int): ResolvedPos? {
-  try {
-    return resolve(pos)
-  } catch (e: RangeError) {
-    // It's safe to log Node.toString as UGC is not returned in Debug mode
-    // TODO: add library to log
-    return null
-  }
+    try {
+        return resolve(pos)
+    } catch (e: RangeError) {
+        if (!safeMode) throw e
+        return null
+    }
 }
