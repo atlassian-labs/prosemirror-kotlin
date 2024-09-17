054b1ebdcd4572a93c842eff3881c5326166dfd1
Aleksei Chernykh <achernykh@atlassian.com>
2024-09-17 18:11:37 +1000

    Generate and upload package.swift file

---
+++ gradle/pack_xcframework.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+module=$1
+file_name=${module//[-]/_}
+tag=$2
+echo $module
+echo $tag
+zip -r ${module}/build/${file_name}.xcframework.zip ${module}/build/XCFrameworks/release/${file_name}.xcframework
+# Prepare Package.swift file
+CHECKSUM=`swift package compute-checksum ${module}/build/${file_name}.xcframework.zip`
+
+export MODULE=$module
+export CHECKSUM=$CHECKSUM
+export XCFRAMEWORK_URL="https://github.com/atlassian-labs/prosemirror-kotlin/releases/${tag}/${file_name}.xcframework.zip"
+cat gradle/templates/package.swift.template | envsubst '$MODULE $CHECKSUM $XCFRAMEWORK_URL' > ${module}/build/Package.swift
