00771ad5fa991b499fd603f8454683020118abf5
Aleksei Chernykh <achernykh@atlassian.com>
2024-09-19 10:59:49 +1000

    Fix url for artifact inside Package.swift files

--- gradle/pack_xcframework.sh
+++ gradle/pack_xcframework.sh
@@ -2,8 +2,6 @@
 module=$1
 file_name=${module//[-]/_}
 tag=$2
-echo $module
-echo $tag
 zip -r ${module}/build/${file_name}.xcframework.zip ${module}/build/XCFrameworks/release/${file_name}.xcframework
 # Prepare Package.swift file
 CHECKSUM=`swift package compute-checksum ${module}/build/${file_name}.xcframework.zip`
