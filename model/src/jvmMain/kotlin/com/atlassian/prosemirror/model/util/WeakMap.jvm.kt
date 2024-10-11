ae5876075201f10e501a87a6382d267f14c96962
Nicole Lam <nlam@atlassian.com>
2024-10-11 13:15:15 +1100

    Custom platform implementation of JS WeakMap

---
+++ model/src/jvmMain/kotlin/com/atlassian/prosemirror/model/util/WeakMap.jvm.kt
@@ -0,0 +1,16 @@
+package com.atlassian.prosemirror.model.util
+
+import java.util.WeakHashMap
+
+class AndroidWeakMap<K, V>: WeakMap<K, V> {
+    private val map = WeakHashMap<K, V>()
+
+    override fun get(key: K): V? = map[key]
+
+    override fun put(key: K, value: V) {
+        map[key] = value
+    }
+
+}
+
+actual fun <K, V> mutableWeakMapOf(): WeakMap<K, V> = AndroidWeakMap()
