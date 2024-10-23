1e47fd1639b9e14cd103ba9f6327c950d4ee9109
Nicole Lam <nlam@atlassian.com>
2024-10-23 13:29:11 +1100

    Move dokka, publishing and signing to top level

--- history/build.gradle.kts
+++ history/build.gradle.kts
@@ -4,9 +4,6 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 plugins {
   alias(libs.plugins.kotlinMultiplatform)
   alias(libs.plugins.ktlint)
-  alias(libs.plugins.dokka)
-  id("maven-publish")
-  id("signing")
 }
 
 kotlin {
@@ -48,103 +45,9 @@ kotlin {
       implementation(libs.test.assertk)
     }
   }
-
-  tasks.dokkaHtml {
-    dokkaSourceSets {
-      val commonMain by getting {
-        sourceLink {
-          // Unix based directory relative path to the root of the project (where you execute gradle respectively).
-          localDirectory.set(file("src/commonMain/kotlin"))
-
-          // URL showing where the source code can be accessed through the web browser
-          remoteUrl.set(URL("https://github.com/atlassian-labs/prosemirror-kotlin/history/src/main/src/commonMain/kotlin"))
-
-          // Suffix which is used to append the line number to the URL. Use #L for GitHub
-          remoteLineSuffix.set("#lines-")
-        }
-      }
-
-      val jvmMain by getting {
-        sourceLink {
-          // Unix based directory relative path to the root of the project (where you execute gradle respectively).
-          localDirectory.set(file("src/jvmMain/kotlin"))
-
-          // URL showing where the source code can be accessed through the web browser
-          remoteUrl.set(URL("https://github.com/atlassian-labs/prosemirror-kotlin/history/src/main/src/jvmMain/kotlin"))
-
-          // Suffix which is used to append the line number to the URL. Use #L for GitHub
-          remoteLineSuffix.set("#lines-")
-        }
-      }
-
-      val nativeMain by getting {
-        sourceLink {
-          // Unix based directory relative path to the root of the project (where you execute gradle respectively).
-          localDirectory.set(file("src/nativeMain/kotlin"))
-
-          // URL showing where the source code can be accessed through the web browser
-          remoteUrl.set(URL("https://github.com/atlassian-labs/prosemirror-kotlin/history/src/main/src/nativeMain/kotlin/"))
-
-          // Suffix which is used to append the line number to the URL. Use #L for GitHub
-          remoteLineSuffix.set("#lines-")
-        }
-      }
-    }
-  }
 }
 
 description = "prosemirror-history"
-
-publishing {
-  publications {
-    publications.withType<MavenPublication> {
-      pom {
-        name.set(project.name)
-        description.set("Undo history for ProseMirror")
-        url.set("https://github.com/atlassian-labs/prosemirror-kotlin/tree/history/")
-
-        scm {
-          connection.set("git@github.com:atlassian-labs/prosemirror-kotlin.git")
-          url.set("https://github.com/atlassian-labs/prosemirror-kotlin.git")
-        }
-        developers {
-          developer {
-            id.set("dmarques")
-            name.set("Douglas Marques")
-            email.set("dmarques@atlassian.com")
-          }
-          developer {
-            id.set("achernykh")
-            name.set("Aleksei Chernykh")
-            email.set("achernykh@atlassian.com")
-          }
-        }
-        licenses {
-          license {
-            name.set("Apache License 2.0")
-            url.set("https://www.apache.org/licenses/LICENSE-2.0")
-            distribution.set("repo")
-          }
-        }
-      }
-    }
-  }
-
-  repositories {
-    maven {
-      url = uri("https://packages.atlassian.com/maven-central")
-      credentials {
-        username = System.getenv("ARTIFACTORY_USERNAME")
-        password = System.getenv("ARTIFACTORY_API_KEY")
-      }
-    }
-  }
-}
-
-signing {
-  useInMemoryPgpKeys(
-    System.getenv("SIGNING_KEY"),
-    System.getenv("SIGNING_PASSWORD"),
-  )
-  sign(publishing.publications)
-}
+ext.set("pomDescription", "Undo history for ProseMirror")
+ext.set("pomUrl", "https://github.com/atlassian-labs/prosemirror-kotlin/tree/history/")
+ext.set("dokkaUrlPrefix", "https://github.com/atlassian-labs/prosemirror-kotlin/history/")
